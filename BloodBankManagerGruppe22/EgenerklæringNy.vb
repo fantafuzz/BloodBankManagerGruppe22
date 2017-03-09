867c009bb2bc1549664f8595d601cff212853dfc
nareny <yogarajahnaren@outlook.com>
2017-03-09 13:03:29 +0100

    variabler lagt inn klasse egenerklæring

---
+++ BloodBankManagerGruppe22/EgenerklæringNy.vb
@@ -0,0 +1,83 @@
+﻿Public Class EgenerklæringNy
+    'Bolk 1: Vennligst besvar
+    Dim infoBlodgivning As Boolean
+    Dim følerFrisk As Boolean
+    Dim friskForrigeperiode As Boolean
+    Dim femtiKgEllerMer As Boolean
+    Dim sårEksemHudsykdom As Boolean
+    Dim piercingSlimhinne As Boolean
+
+    'Bolk 2: Har du i løpet av de siste fire uker
+    Dim bruktMedisiner As Boolean
+    Dim værtSykEllerHattFeber As Boolean
+    Dim hattLøsAvføring As Boolean
+    Dim fåttVaksine As Boolean
+    Dim værtHosTannlegeEllerTannpleier As Boolean
+
+    'Bolk 3: Har du i løpet av de siste seks måneder
+    Dim legeUndersøkelseEllerBehandlingForSykdom As Boolean
+    Dim behandlingMedSprøyter As Boolean
+    Dim kjønnsykdomEllerfåttBehandlingForKjønnsykdom As Boolean
+    Dim kjønnsykdomSeksuellKontaktHivHepatittBEllerC
+    Dim kjønnsykdomSeksuellKontaktDopingmidlerNarkotiskemidler As Boolean
+    Dim hattSeksuellKontaktMedProstituerteEllerTidligereProstituerte As Boolean
+    Dim tattHullIØretFåttPiercing As Boolean
+
+    'Bolk 4: Har du i løpet av de siste seks måneder
+    Dim akupunktur As Boolean
+    Dim stukketEllerSåretDegPåGjenstanderSomVarForurensetMedBlodEllerKropsvæske As Boolean
+    Dim boddISammeHusstandSomEnPersonSomHarHepatittB As Boolean
+    Dim fåttBlodsølPåSlimhinnerEllerSkadetHud As Boolean
+    Dim harBlittBittAvFlått As Boolean
+    Dim hattSeksuellPartnerVestEuropa As Boolean
+    Dim hattSeksuellPartnerAfrika As Boolean
+    Dim hattSeksuellKontaktBlodEllerBlodprodukterUtenforNorden As Boolean
+    Dim hattNySeksuellPartner As Boolean
+    Dim værtUtenforVestEuropa As Boolean
+
+    'Bolk 5: Har du i løpet av de siste to år
+    Dim hattSjeldenEllerAlvorligeInfeksjonssykdommer As Boolean
+
+    'Bolk 6: Har du på noe tidspunkt gjennom livet
+    Dim hjerteLeverLungeSykdom As Boolean
+    Dim hattKreft As Boolean
+    Dim hattBlødningstendens As Boolean
+    Dim hattAllergiMotMatEllerMedisiner As Boolean
+    Dim hattMalaria As Boolean
+    Dim hattTropesykdommer As Boolean
+    Dim hattHepatittHivAids As Boolean
+    Dim positivPrøveHepatittHivinfeksjon As Boolean
+    Dim blodOverføring As Boolean
+    Dim vekstHormon As Boolean
+    Dim hornHinnetransplantant As Boolean
+    Dim syflis As Boolean
+    Dim alvorligSykdomSomIkkeErNevntHer As Boolean
+    Dim bruktDopingmidlerEllernarktoiskeStoffer As Boolean
+    Dim mottatPengerEllerNarkotikaSomGjenytelse As Boolean
+
+    'Bolk 7: Besvares av kvinner
+    Dim erDuGravid As Boolean
+    Dim værtGravidEllerAmmer As Boolean
+    Dim værtGravidSidenSisteBlodgivning As Boolean
+    Dim seksuellKontaktMedMannSomHarHattSeksuellKontaktMedAndreMenn As Boolean
+
+    'Bolk 8: Besvares av menn
+    Dim harEllerHarDuHattSeksuellKontaktMedAndreMenn As Boolean
+
+    'Bolk 9: Besvar også
+    Dim bruktNarkotikaDeSisteTolvMåneder As Boolean
+    Dim hattCJD As Boolean
+    Dim storbritaniaOpphold As Boolean
+    Dim områdeMalariaTreÅr As Boolean
+    Dim områdeMalariaSeksMåneder As Boolean
+    Dim afrikaOppholdMerEnnFemår As Boolean
+    Dim fødtISørForUSA As Boolean
+    Dim annonymisertePrøverForskning As Boolean
+    Dim medikamentForsøk As Boolean
+    Dim mittPlasmaUtAvNorgeLegemiddelproduksjon As Boolean
+
+    'Bolk 10: Eventuelle andre opplysninger
+    Dim eventuelleAndreOpplysninger As String
+    Dim tillatelseBlodgiverEpostSms As String
+
+End Class
