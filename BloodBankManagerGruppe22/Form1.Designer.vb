0644002cb9a068818157169247011aabd06230b0
nareny <yogarajahnaren@outlook.com>
2017-03-09 14:14:44 +0100

    lagt inn gammel kode

--- BloodBankManagerGruppe22/Form1.Designer.vb
+++ BloodBankManagerGruppe22/Form1.Designer.vb
@@ -22,72 +22,183 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
-        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
         Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBoxRegPword = New System.Windows.Forms.TextBox()
+        Me.TextBoxRegUsername = New System.Windows.Forms.TextBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBoxPassword = New System.Windows.Forms.TextBox()
+        Me.TextBoxUsername = New System.Windows.Forms.TextBox()
+        Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
-        'Button1
+        'Label3
         '
-        Me.Button1.Location = New System.Drawing.Point(265, 144)
-        Me.Button1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(100, 28)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "PLS WORK"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(476, 91)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(85, 17)
+        Me.Label3.TabIndex = 15
+        Me.Label3.Text = "Registrering"
         '
-        'CheckBox1
+        'Panel1
         '
-        Me.CheckBox1.AutoSize = True
-        Me.CheckBox1.Location = New System.Drawing.Point(239, 241)
-        Me.CheckBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
-        Me.CheckBox1.Name = "CheckBox1"
-        Me.CheckBox1.Size = New System.Drawing.Size(100, 21)
-        Me.CheckBox1.TabIndex = 1
-        Me.CheckBox1.Text = "CheckBox1"
-        Me.CheckBox1.UseVisualStyleBackColor = True
-        '
-        'CheckBox2
-        '
-        Me.CheckBox2.AutoSize = True
-        Me.CheckBox2.Location = New System.Drawing.Point(239, 270)
-        Me.CheckBox2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
-        Me.CheckBox2.Name = "CheckBox2"
-        Me.CheckBox2.Size = New System.Drawing.Size(100, 21)
-        Me.CheckBox2.TabIndex = 2
-        Me.CheckBox2.Text = "CheckBox2"
-        Me.CheckBox2.UseVisualStyleBackColor = True
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel1.Controls.Add(Me.Button2)
+        Me.Panel1.Controls.Add(Me.Label5)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.TextBoxRegPword)
+        Me.Panel1.Controls.Add(Me.TextBoxRegUsername)
+        Me.Panel1.Location = New System.Drawing.Point(393, 110)
+        Me.Panel1.Margin = New System.Windows.Forms.Padding(4)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(265, 147)
+        Me.Panel1.TabIndex = 16
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(252, 358)
+        Me.Button2.Location = New System.Drawing.Point(4, 105)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(4)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 23)
-        Me.Button2.TabIndex = 3
-        Me.Button2.Text = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(100, 28)
+        Me.Button2.TabIndex = 10
+        Me.Button2.Text = "Utfør"
         Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(4, 53)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(60, 17)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Passord"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(4, 5)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(81, 17)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Brukernavn"
+        '
+        'TextBoxRegPword
+        '
+        Me.TextBoxRegPword.Location = New System.Drawing.Point(4, 73)
+        Me.TextBoxRegPword.Margin = New System.Windows.Forms.Padding(4)
+        Me.TextBoxRegPword.Name = "TextBoxRegPword"
+        Me.TextBoxRegPword.Size = New System.Drawing.Size(200, 22)
+        Me.TextBoxRegPword.TabIndex = 7
+        '
+        'TextBoxRegUsername
+        '
+        Me.TextBoxRegUsername.Location = New System.Drawing.Point(4, 25)
+        Me.TextBoxRegUsername.Margin = New System.Windows.Forms.Padding(4)
+        Me.TextBoxRegUsername.Name = "TextBoxRegUsername"
+        Me.TextBoxRegUsername.Size = New System.Drawing.Size(200, 22)
+        Me.TextBoxRegUsername.TabIndex = 6
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 16
+        Me.ListBox1.Location = New System.Drawing.Point(112, 296)
+        Me.ListBox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(159, 116)
+        Me.ListBox1.TabIndex = 14
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(28, 182)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 28)
+        Me.Button1.TabIndex = 13
+        Me.Button1.Text = "Logg inn"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(24, 118)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(60, 17)
+        Me.Label2.TabIndex = 12
+        Me.Label2.Text = "Passord"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(24, 56)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(81, 17)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "Brukernavn"
+        '
+        'TextBoxPassword
+        '
+        Me.TextBoxPassword.Location = New System.Drawing.Point(28, 137)
+        Me.TextBoxPassword.Margin = New System.Windows.Forms.Padding(4)
+        Me.TextBoxPassword.Name = "TextBoxPassword"
+        Me.TextBoxPassword.Size = New System.Drawing.Size(212, 22)
+        Me.TextBoxPassword.TabIndex = 10
+        Me.TextBoxPassword.UseSystemPasswordChar = True
+        '
+        'TextBoxUsername
+        '
+        Me.TextBoxUsername.Location = New System.Drawing.Point(28, 76)
+        Me.TextBoxUsername.Margin = New System.Windows.Forms.Padding(4)
+        Me.TextBoxUsername.Name = "TextBoxUsername"
+        Me.TextBoxUsername.Size = New System.Drawing.Size(212, 22)
+        Me.TextBoxUsername.TabIndex = 9
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(683, 468)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.CheckBox2)
-        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.ListBox1)
         Me.Controls.Add(Me.Button1)
-        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBoxPassword)
+        Me.Controls.Add(Me.TextBoxUsername)
+        Me.Margin = New System.Windows.Forms.Padding(4)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents Button1 As Button
-    Friend WithEvents CheckBox1 As CheckBox
-    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Panel1 As Panel
     Friend WithEvents Button2 As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBoxRegPword As TextBox
+    Friend WithEvents TextBoxRegUsername As TextBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBoxPassword As TextBox
+    Friend WithEvents TextBoxUsername As TextBox
 End Class
--- BloodBankManagerGruppe22/Form1.vb
+++ BloodBankManagerGruppe22/Form1.vb
@@ -1,3 +1,113 @@
 ﻿Public Class Form1
-    'test ny versjon
+    Imports MySql.Data.MySqlClient
+    Public Class Form1
+        Public currentuser As Integer
+        Private salt As String = "detteErEnVeldigLangHashSomGjorAtVaarePassordBlirMyeSikrere"
+        Private username As String
+        Private password As String
+        Private databasename As String
+        Private server As String =
+        Private userinfo As New DataTable
+
+        Private connstring As String
+        Private connection As New MySqlConnection()
+
+        Private Function hash(passord As String, salt As String) As String
+            Dim hashObject = New Security.Cryptography.SHA256Managed()
+            Dim bytes = System.Text.Encoding.ASCII.GetBytes(passord & salt)
+            bytes = hashObject.ComputeHash(bytes)
+
+            Dim hexString As String = ""
+            For Each aByte In bytes
+                hexString &= aByte.ToString("x2")
+            Next
+
+            Return hexString
+        End Function
+        Private Function Query(ByVal sql As String) As DataTable
+            Dim table As New DataTable
+            Try
+                connection.Open()
+
+                ' Do the query, and fill a table with data
+
+                Dim command As New MySqlCommand(sql, connection)
+
+                Dim da As New MySqlDataAdapter
+                da.SelectCommand = command
+                da.Fill(table)
+
+                connection.Close()
+
+            Catch ex As MySqlException
+                MessageBox.Show("En feil oppstått - " & ex.Message)
+            End Try
+            Return table
+        End Function
+
+        Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+            connstring = "Server=" & server & ";" & "Database=" & databasename & ";" & "Uid=" & username & ";" & "Pwd=" & password & ";"
+            connection.ConnectionString = connstring
+            userinfo = Query("SELECT bruker_id, brukernavn, passord FROM bruker")
+            For Each row As DataRow In userinfo.Rows
+                ListBox1.Items.Add(row("bruker_id") & " " & row("brukernavn") & " " & row("passord"))
+            Next
+        End Sub
+
+        Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+            Dim gotmatch As Boolean = False
+            Dim inputname As String = TextBoxUsername.Text
+            Dim inputpword As String = TextBoxPassword.Text
+            Dim hashedpword As String = hash(inputpword, salt)
+
+            For Each row As DataRow In userinfo.Rows
+                If row("brukernavn") = inputname And row("passord") = hashedpword Then
+                    gotmatch = True
+                    currentuser = row("bruker_id")
+                End If
+            Next
+            If gotmatch Then
+                MsgBox(currentuser)
+            End If
+        End Sub
+
+        Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+            Dim inputname As String = TextBoxRegUsername.Text
+            Dim inputpword As String = TextBoxRegPword.Text
+            inputpword = hash(inputpword, salt)
+            MsgBox(inputpword)
+
+            If inputname <> "" And inputpword <> "" Then
+                Query("INSERT INTO `login_form_forside`(`brukernavn`, `passord`) VALUES ('" & inputname & "','" & inputpword & "');")
+                'Query("INSERT INTO `login_form_forside`(`brukernavn`, `passord`) VALUES ('ola','hallo')")
+            End If
+        End Sub
+
+        '    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        '   Dim tabell As New DataTable
+        '      ListBox1.Items.Clear()
+        '     tabell = Sporring("SELECT * FROM person ORDER BY fornavn")
+        'For Each rad As DataRow In tabell.Rows
+        '       ListBox1.Items.Add(rad("person_id") & " " & rad("fornavn") & " " & rad("etternavn") & rad("epost") & rad("dato"))
+        'Next
+        'End Sub
+
+        '   Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        '  Dim tabell As New DataTable
+        '     ListBox1.Items.Clear()
+        '    tabell = Sporring("SELECT * FROM person WHERE fornavn='" & txtPerson.Text & "' OR etternavn='" & txtPerson.Text & "';")
+        'For Each rad As DataRow In tabell.Rows
+        '       ListBox1.Items.Add(rad("person_id") & " " & rad("fornavn") & " " & rad("etternavn") & " " & rad("epost") & " " & rad("dato"))
+        'Next
+        'End Sub
+
+        '    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        '   Dim tabell As New DataTable
+        '      ListBox1.Items.Clear()
+        '     tabell = Sporring("SELECT * FROM person WHERE epost LIKE '%" & txtEpost.Text & "%';")
+        'For Each rad As DataRow In tabell.Rows
+        '       ListBox1.Items.Add(rad("person_id") & " " & rad("fornavn") & " " & rad("etternavn") & " " & rad("epost") & " " & rad("dato"))
+        'Next
+        'End Sub
+    End Class
 End Class
