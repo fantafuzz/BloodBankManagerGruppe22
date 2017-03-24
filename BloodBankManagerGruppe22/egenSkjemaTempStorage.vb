4a870e4f67ccffe1f2c2513acd1357caab97a82c
Marius Myhre <mariusibenfeldtmyhre@gmail.com>
2017-03-24 08:07:31 +0100

    Startet på Egenskjema V2 (egenskjematempstorage)

--- BloodBankManagerGruppe22/egenSkjemaTempStorage.vb
+++ BloodBankManagerGruppe22/egenSkjemaTempStorage.vb
@@ -1,9 +1,4 @@
 ﻿Public Class egenSkjemaTempStorage
-    Public svar(60) As Boolean
-    Private Sub egenSkjemaTempStorage_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-        For Each s In svar
-            ListBox1.Items.Add(s)
-        Next
-    End Sub
+
 End Class
