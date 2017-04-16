a7962a44f29b9b896af1365f1ef61de15fe15f49
nareny <yogarajahnaren@outlook.com>
2017-04-16 10:49:44 +0200

    Design

--- BloodBankManagerGruppe22/helseSjekkBlodgiver.Designer.vb
+++ BloodBankManagerGruppe22/helseSjekkBlodgiver.Designer.vb
@@ -36,21 +36,21 @@ Partial Class helseSjekkBlodgiver
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(22, 45)
+        Me.Label1.Location = New System.Drawing.Point(13, 42)
         Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(414, 32)
+        Me.Label1.Size = New System.Drawing.Size(322, 26)
         Me.Label1.TabIndex = 3
         Me.Label1.Text = "Måling av hemoglobinverdien"
         '
         'DataGridView1
         '
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(28, 81)
-        Me.DataGridView1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.DataGridView1.Location = New System.Drawing.Point(18, 72)
+        Me.DataGridView1.Margin = New System.Windows.Forms.Padding(4)
         Me.DataGridView1.Name = "DataGridView1"
         Me.DataGridView1.RowTemplate.Height = 24
-        Me.DataGridView1.Size = New System.Drawing.Size(1091, 552)
+        Me.DataGridView1.Size = New System.Drawing.Size(1318, 625)
         Me.DataGridView1.TabIndex = 2
         '
         'MenuStrip1
@@ -60,20 +60,20 @@ Partial Class helseSjekkBlodgiver
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
         Me.MenuStrip1.Padding = New System.Windows.Forms.Padding(9, 3, 0, 3)
-        Me.MenuStrip1.Size = New System.Drawing.Size(1231, 30)
+        Me.MenuStrip1.Size = New System.Drawing.Size(1347, 25)
         Me.MenuStrip1.TabIndex = 4
         Me.MenuStrip1.Text = "MenuStrip1"
         '
         'HvaErHemoglobinToolStripMenuItem
         '
         Me.HvaErHemoglobinToolStripMenuItem.Name = "HvaErHemoglobinToolStripMenuItem"
-        Me.HvaErHemoglobinToolStripMenuItem.Size = New System.Drawing.Size(156, 24)
+        Me.HvaErHemoglobinToolStripMenuItem.Size = New System.Drawing.Size(126, 19)
         Me.HvaErHemoglobinToolStripMenuItem.Text = "Hva er hemoglobin?"
         '
         'Logg_ut
         '
         Me.Logg_ut.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Logg_ut.Location = New System.Drawing.Point(895, 675)
+        Me.Logg_ut.Location = New System.Drawing.Point(1009, 722)
         Me.Logg_ut.Name = "Logg_ut"
         Me.Logg_ut.Size = New System.Drawing.Size(203, 42)
         Me.Logg_ut.TabIndex = 31
@@ -83,7 +83,7 @@ Partial Class helseSjekkBlodgiver
         'Button1
         '
         Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button1.Location = New System.Drawing.Point(1104, 675)
+        Me.Button1.Location = New System.Drawing.Point(1218, 722)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(118, 42)
         Me.Button1.TabIndex = 32
@@ -92,10 +92,10 @@ Partial Class helseSjekkBlodgiver
         '
         'helseSjekkBlodgiver
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 24.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 18.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.White
-        Me.ClientSize = New System.Drawing.Size(1231, 729)
+        Me.ClientSize = New System.Drawing.Size(1347, 776)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Logg_ut)
         Me.Controls.Add(Me.Label1)
@@ -103,7 +103,7 @@ Partial Class helseSjekkBlodgiver
         Me.Controls.Add(Me.MenuStrip1)
         Me.Font = New System.Drawing.Font("Georgia", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.MainMenuStrip = Me.MenuStrip1
-        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Margin = New System.Windows.Forms.Padding(4)
         Me.Name = "helseSjekkBlodgiver"
         Me.Text = "Blood Bank Administrator 2k17™ - xTreme Edition"
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
